fbb6bb3db2d76e8597a0f478600f292912cad25f
yilong <lucky-ilong@sohu.com>
2025-05-13 12:38:38 +0800

    fix url encoding error

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ import os
 load_dotenv()  # read .env file
 
 LLM_API_KEY = os.getenv("LLM_API_KEY")
-LLM_BASE_URL = os.getenv("LLM_BASE_URL")
+LLM_BASE_URL = os.getenv("LLM_BASE_URL").replace("\\x3a", ":")
